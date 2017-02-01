338c9f22ec676826ac03e0506b1e9f6cfaf6814a
Hans Lee <hansclee@gmail.com>
2017-02-01 00:04:15 +0000

    Commiting test

--- tests2.py
+++ tests2.py
@@ -21,30 +21,37 @@ pwm = Adafruit_PCA9685.PCA9685()
 
 # Configure min and max servo pulse lengths
 servo_min = 150  # Min pulse length out of 4096
-servo_max = 600  # Max pulse length out of 4096
+servo_max = 542  # Max pulse length out of 4096
 
 # Helper function to make setting a servo pulse width simpler.
 def set_servo_pulse(channel, pulse):
-    pulse_length = 1000000    # 1,000,000 us per second
-    pulse_length //= 60       # 60 Hz
-    print('{0}us per period'.format(pulse_length))
-    pulse_length //= 4096     # 12 bits of resolution
-    print('{0}us per bit'.format(pulse_length))
+    pulse_length = 1000000.0    # 1,000,000 us per second
+    pulse_length /= 50.       # 50 Hz
+    print(str(pulse_length) + ' us per period')
+    pulse_length /= 4096.     # 12 bits of resolution
+    print(str(round(pulse_length)) + ' us per bit')
     pulse *= 1000
     pulse //= pulse_length
     print "pulse: " + str(pulse)
-    #pwm.set_pwm(channel, 0, pulse)
+    pwm.set_pwm(channel, 0, int(pulse))
 
 # Set frequency to 60hz, good for servos.
-pwm.set_pwm_freq(60)
+pwm.set_pwm_freq(50)
 
-set_servo_pulse(0, 100)
-
+set_servo_pulse(0, 1.5)
 exit()
 print('Moving servo on channel 0, press Ctrl-C to quit...')
-while True:
-    # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_min)
-    time.sleep(1)
-    pwm.set_pwm(0, 0, servo_max)
-    time.sleep(1)
+#while True:
+# Move servo on channel O between extremes.
+#set_servo_pulse(0, 0.5)
+pwm.set_pwm(0, 0, 102)
+time.sleep(1)
+#set_servo_pulse(0, 2.5)
+pwm.set_pwm(0, 0, 512)
+time.sleep(1)
+#set_servo_pulse(0, 1.5)
+pwm.set_pwm(0, 0, 312)
+time.sleep(4)
+
+
+  # 521, 104, 312
